2f213772b434e228b61e844cf47c39dd3c360ba0
skeller1 <MiStK@gmx.de>
2013-08-14 18:04:31 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuss::Application.config.secret_key_base = '4195f56b8a7af13b6260354c22d026cdb240c943575876fdf9f26497a7c492e2ff468f0e115abd075feffadec09bd8e1721ecc97e081b59bd93541788f3023e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fuss::Application.config.session_store :cookie_store, key: '_fuss_session'
